24310dfb0f50eebc8473506c9a6e5b228808d2d8
 Katia S <katesakowicz@MacBook-Pro-4.local>
2021-04-29 16:19:20 +0200

    last changes

--- slider/slider.js
+++ slider/slider.js
@@ -1,3 +1,4 @@
+//classNames
 const SliderClassName = 'slider';
 const SliderContainerLineClassName = 'slider-container-line';
 const SliderLineClassName = 'slider-line';
@@ -7,6 +8,8 @@ const SliderNavLeftClassName = 'slider-nav-left';
 const SliderNavRightClassName = 'slider-nav-right';
 const SliderNavDisabledClassName = 'slider-nav-disabled';
 const SliderDragClassName = 'slider-drag';
+
+//object creation and initialization of Slider class with constructor method
 class Slider {
   constructor(element) {
     this.containerNode = element;
@@ -25,13 +28,13 @@ class Slider {
     this.shiftLeft = this.shiftLeft.bind(this);
     this.shiftRight = this.shiftRight.bind(this);
     this.changeCurrentImg = this.changeCurrentImg.bind(this);
-    this.changeDisabledNav = this.changeDisabledNav.bind(this);
+    this.changeNav = this.changeNav.bind(this);
 
     this.setHTML();
     this.setParameters();
     this.setEvents();
   }
-
+  //adding new html elements with class attributes
   setHTML() {
     this.containerNode.classList.add(SliderClassName);
     this.containerNode.innerHTML = `
@@ -46,12 +49,13 @@ class Slider {
              </div>
           `;
 
+    // getting elements by classNames
     this.lineContainerNode = this.containerNode.querySelector(
       `.${SliderContainerLineClassName}`
     );
     this.lineNode = this.containerNode.querySelector(`.${SliderLineClassName}`);
 
-    //create array of divs with className that wrap images
+    //create array of divs with specific className that wrap images
     this.slideNodes = Array.from(this.lineNode.children).map((childNode) =>
       wrapElement({
         element: childNode,
@@ -66,15 +70,15 @@ class Slider {
       `.${SliderNavRightClassName}`
     );
   }
-  //set parameters for slider container and images to manipulate their width
+  //set parameters for slider's container and images to manipulate their width
   setParameters() {
-    const containerLinePosition = this.lineContainerNode.getBoundingClientRect();
+    const containerLinePosition = this.lineContainerNode.getBoundingClientRect(); // getting position(width) of slider container relative to the viewport
     this.width = containerLinePosition.width;
-    this.x = -this.currentImg * this.width;
-    this.resetTransition();
-    this.lineNode.style.width = `${this.size * this.width}px`;
+    this.x = -this.currentImg * this.width; //setting an image shift
+    this.lineNode.style.width = `${this.size * this.width}px`; //getting line width for all images
     this.setPosition();
-    this.changeDisabledNav();
+    this.changeNav();
+    this.resetTransition();
 
     //set width for each image
     Array.from(this.slideNodes).forEach((slideNode) => {
@@ -85,21 +89,13 @@ class Slider {
   //add event listeners
   setEvents() {
     window.addEventListener('resize', this.resizeSlider);
-    this.lineNode.addEventListener('pointerdown', this.startDrag);
+    this.lineNode.addEventListener('pointerdown', this.startDrag); // pointer works for both mouse and finger clicks
     window.addEventListener('pointerup', this.stopDrag);
     window.addEventListener('pointercancel', this.stopDrag);
     this.navLeft.addEventListener('click', this.shiftLeft);
     this.navRight.addEventListener('click', this.shiftRight);
   }
-  //clean up event listeners to avoid memory leaks
-  removeEvents() {
-    window.removeEventListener('resize', this.resizeSlider);
-    this.lineNode.removeEventListener('pointerdown', this.startDrag);
-    window.removeEventListener('pointerup', this.stopDrag);
-    window.removeEventListener('pointercancel', this.stopDrag);
-    this.navLeft.removeEventListener('click', this.shiftLeft);
-    this.navRight.removeEventListener('click', this.shiftRight);
-  }
+
   //recalculate the width of the slider on screen resizing
   resizeSlider() {
     this.setParameters();
@@ -108,30 +104,30 @@ class Slider {
   startDrag(e) {
     this.currentImgChanged = false; //condition for an image shift
     this.clickX = e.pageX;
-    this.startX = this.x;
+    this.startX = this.x; //initial position when start dragging
     this.resetTransition();
     this.containerNode.classList.add(SliderDragClassName);
     window.addEventListener('pointermove', this.drag);
   }
-
+  //all calculations on drag event
   drag(e) {
-    this.dragX = e.pageX;
-    const shift = this.dragX - this.clickX;
-    this.x = this.startX + shift;
+    this.dragX = e.pageX; //horizontal drag
+    const shift = this.dragX - this.clickX; //detect drag shift
+    this.x = this.startX + shift; //adding shift while dragging
     this.setPosition();
 
-    // Change current image
+    // change current image on drag
     if (
-      shift > 20 && //if shift less than 20px, ignore it
+      shift > 30 && //if shift is less than 30px, ignore it
       shift > 0 &&
-      !this.currentImgChanged &&
+      !this.currentImgChanged && //check one image change per drag
       this.currentImg > 0
     ) {
       this.currentImgChanged = true;
       this.currentImg = this.currentImg - 1;
     }
     if (
-      shift < -20 &&
+      shift < -30 && // ignore if shift is less then -30px
       shift < 0 &&
       !this.currentImgChanged &&
       this.currentImg < this.size - 1
@@ -146,7 +142,7 @@ class Slider {
     this.containerNode.classList.remove(SliderDragClassName);
     this.changeCurrentImg();
   }
-
+  //buttons right/left clicks
   shiftLeft() {
     if (this.currentImg <= 0) {
       return;
@@ -162,28 +158,8 @@ class Slider {
     this.currentImg = this.currentImg + 1;
     this.changeCurrentImg();
   }
-
-  changeCurrentImg() {
-    this.x = -this.currentImg * this.width;
-    this.setTransition();
-    this.setPosition();
-    this.changeDisabledNav();
-  }
-
-  //change position on drag
-  setPosition() {
-    this.lineNode.style.transform = `translate3d(${this.x}px, 0, 0)`;
-  }
-
-  setTransition() {
-    this.lineNode.style.transition = `all 0.25s ease 0s`;
-  }
-
-  resetTransition() {
-    this.lineNode.style.transition = `all 0s ease 0s`;
-  }
-
-  changeDisabledNav() {
+  // disable buttons based on current image
+  changeNav() {
     if (this.currentImg <= 0) {
       this.navLeft.classList.add(SliderNavDisabledClassName);
     } else {
@@ -195,13 +171,42 @@ class Slider {
       this.navRight.classList.remove(SliderNavDisabledClassName);
     }
   }
-}
-//Helper function to wrap each image element in div
-function wrapElement({ element, className }) {
-  const wrapperNode = document.createElement('div');
-  wrapperNode.classList.add(className);
-  element.parentNode.insertBefore(wrapperNode, element);
-  wrapperNode.appendChild(element);
 
-  return wrapperNode;
+  //change image on button click or on drag/shift
+  changeCurrentImg() {
+    this.x = -this.currentImg * this.width; //recalculate the width to shift an image
+    this.setPosition();
+    this.setTransition();
+    this.changeNav();
+  }
+  //change position on drag
+  setPosition() {
+    this.lineNode.style.transform = `translate3d(${this.x}px, 0, 0)`;
+  }
+  // smooth transition change
+  setTransition() {
+    this.lineNode.style.transition = `all 0.25s ease 0s`;
+  }
+
+  resetTransition() {
+    this.lineNode.style.transition = `all 0s ease 0s`;
+  }
+
+  //clean up event listeners to avoid memory leaks
+  removeEvents() {
+    window.removeEventListener('resize', this.resizeSlider);
+    this.lineNode.removeEventListener('pointerdown', this.startDrag);
+    window.removeEventListener('pointerup', this.stopDrag);
+    window.removeEventListener('pointercancel', this.stopDrag);
+    this.navLeft.removeEventListener('click', this.shiftLeft);
+    this.navRight.removeEventListener('click', this.shiftRight);
+  }
+}
+//helper function to wrap each image element in div
+function wrapElement({ element, className }) {
+  const wrapper = document.createElement('div');
+  wrapper.classList.add(className);
+  element.parentNode.insertBefore(wrapper, element);
+  wrapper.appendChild(element);
+  return wrapper;
 }
